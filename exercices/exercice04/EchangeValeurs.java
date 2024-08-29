0924fec24cf79e5005dabc48f1093aafe8f6609e
mihajlovic-filip-emf <filip.mihajovic@studenfr.ch>
2024-08-29 22:08:13 +0200

    fin ex4

--- exercices/exercice04/EchangeValeurs.java
+++ exercices/exercice04/EchangeValeurs.java
@@ -5,7 +5,10 @@ public class EchangeValeurs {
         int variable1 = 1;
         int variable2 =2;
         System.out.println("Le contenu de la variable1 est : " + variable1 + " et celui de la variable2 est : " + variable2);
-        
+        int after = variable1;
+        after = variable1;
+        variable1 = variable2;
+        variable2 = after;
         System.out.println("Le contenu de la variable1 est : " + variable1 + " et celui de la variable2 est : " + variable2);
     }
 }
