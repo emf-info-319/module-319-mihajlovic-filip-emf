462935291f5cb8de8bfaf9f01c670d935c930e86
mihajlovic-filip-emf <y*********@gmail.com>
2024-09-05 10:35:49 +0200

    devoir1

---
+++ devoirs/devoir01/Devoir1.java
@@ -0,0 +1,18 @@
+package devoirs.devoir01;
+
+public class Devoir1 {
+    public static void main(String[] args) {
+        int monAge = 30;
+        int estMajeur = 20;
+
+        System.out.println("Je m'appelle MIHAJLOVIC Filip");
+        System.out.println("Mon age est de : " + monAge + " ans");
+
+        if (monAge>=estMajeur) {
+            System.out.println("Est majeur");
+        }
+        else {
+            System.out.println("Je ne suis pas encore majeur");
+        }
+    }
+}
