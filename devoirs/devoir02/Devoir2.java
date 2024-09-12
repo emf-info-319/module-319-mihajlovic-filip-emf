86751faa8826d57ebaa497889493d0092e567ca9
mihajlovic-filip-emf <y*********@gmail.com>
2024-09-12 11:31:31 +0200

    jeudi

---
+++ devoirs/devoir02/Devoir2.java
@@ -0,0 +1,13 @@
+package devoirs.devoir02;
+
+public class Devoir2 {
+    public final static int CAPACITE_RESERVOIR_A = 3;
+    public final static int CAPACITE_RESERVOIR_B = 5;
+    
+    public static void main(String[] args) {
+        int reservoirA;
+        int reservoirB;
+        int remplissage;
+        
+    }
+}
