27d40689f50f911e6aa77ea101960bd10241c7f3
mihajlovic-filip-emf <y*********@gmail.com>
2024-09-12 12:55:29 +0200

    devoir debut

--- devoirs/devoir02/Devoir2.java
+++ devoirs/devoir02/Devoir2.java
@@ -5,9 +5,13 @@ public class Devoir2 {
     public final static int CAPACITE_RESERVOIR_B = 5;
     
     public static void main(String[] args) {
-        int reservoirA;
-        int reservoirB;
-        int remplissage;
-        
+        int reservoirA = 0;
+        int reservoirB = 0;
+        int remplissage = (int)(Math.random()*(5-0+1))+0;
+
+        do{
+            reservoirA = reservoirA + remplissage;
+        }
+        while ( reservoirA <=CAPACITE_RESERVOIR_A);
     }
 }
