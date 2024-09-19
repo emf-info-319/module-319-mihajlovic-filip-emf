973b125e94b8bc9448edc7a1543a004829ebd598
mihajlovic-filip-emf <y*********@gmail.com>
2024-09-19 11:27:46 +0200

    ex 9

--- exercices/exercice09/Exercice4.java
+++ exercices/exercice09/Exercice4.java
@@ -2,6 +2,16 @@ public class Exercice4 {
     public final static int MIN = 0;
     public final static int MAX = 100;
     public static void main(String[] args) {
+        int[] tab = new tab[3];
         
-    }
+        int num = (int) Math.random() * ( MAX - MIN + 1 ) + MIN;
+
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = num+1;
+            int consecutif = tab[i];
+            System.out.println("Cellule " + i + ":" + consecutif);
+        }
+
+     }
 }
+
