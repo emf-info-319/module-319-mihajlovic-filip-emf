63e024df38b311c352809abc6440ca41fe6d2012
mihajlovic-filip-emf <filip.mihajovic@studenfr.ch>
2024-09-21 09:45:50 +0200

    devoir 3

--- devoirs/devoir03/Devoir3.java
+++ devoirs/devoir03/Devoir3.java
@@ -3,14 +3,14 @@ package devoirs.devoir03;
 
 public class Devoir3 {
     public static void main(String[] args) {
-        int jour = 20;
+        int jour = 21;
         int mois = 9;
         int annee = 2024;
         int[] tab;
         tab = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
         int jourtotal = 0;
         if (annee % 4 == 0 || annee % 400 == 0) {
-            tab[2] = 29;
+            tab[1] = 29;
             
         }
         for (int i = 0; i < mois; i++) {
