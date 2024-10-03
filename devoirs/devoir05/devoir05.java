b6fe6aad5efc8a938dc2a5c5fcc8e8098d86f24d
mihajlovic-filip-emf <y*********@gmail.com>
2024-10-03 12:59:53 +0200

    debut devoir05

---
+++ devoirs/devoir05/devoir05.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * devoir05
+ */
+public class devoir05 {
+    public final static int MAX = 100;
+    public final static int MIN = 0;
+    public static void main(String[] args) {
+        int num = (int) (Math.random() * ( MAX - MIN + 1 )) + MIN;
+        // On crée un scanner pour lire sur la ligne de commande les touches pressées
+Scanner scanner = new Scanner( System.in );
+// Voici comment lire un entier depuis la console
+System.out.print( "Entrez une valeur svp : " );
+int valeur = scanner.nextInt();
+scanner.nextLine(); // pour ignorer la touche RETURN
+// On continue à utiliser le scanner dans le programme…
+// On referme le scanner à la fin du programme
+scanner.close();
+    }
+}
