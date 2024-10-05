024aaeb7f5bec8135638d2a34854438ff9819bcc
mihajlovic-filip-emf <filip.mihajovic@studentfr.ch>
2024-10-05 22:48:38 +0200

    debut devoir 05

--- devoirs/devoir05/devoir05.java
+++ devoirs/devoir05/devoir05.java
@@ -8,7 +8,8 @@ public class devoir05 {
     public final static int MIN = 0;
     public static void main(String[] args) {
         int num = (int) (Math.random() * ( MAX - MIN + 1 )) + MIN;
-        // On crée un scanner pour lire sur la ligne de commande les touches pressées
+
+// On crée un scanner pour lire sur la ligne de commande les touches pressées
 Scanner scanner = new Scanner( System.in );
 // Voici comment lire un entier depuis la console
 System.out.print( "Entrez une valeur svp : " );
