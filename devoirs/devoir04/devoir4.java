2c4d4fe0160e5bdc5a8f0ea49bd2cb2c02155126
mihajlovic-filip-emf <y*********@gmail.com>
2024-10-10 08:57:30 +0200

    devoir 4 ajusté

--- devoirs/devoir04/devoir4.java
+++ devoirs/devoir04/devoir4.java
@@ -17,6 +17,8 @@ public class devoir4 {
                 maxDays = 28;
             }
         }
+        else if (month == 4 || month == 6 || month == 9 || month == 11)
+        maxDays = 30;
         if (year>MAX_YEARS || year<0) {
             System.out.println("L'année [" + year + "] est hors limites");
         }
