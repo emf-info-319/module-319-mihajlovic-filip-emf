de0a589760b59c2a827e44db90e87475e0ba8781
mihajlovic-filip-emf <filip.mihajovic@studentfr.ch>
2024-10-11 09:49:59 +0200

    Ex String

---
+++ exercices/exercice12/ExerciceString.java
@@ -0,0 +1,9 @@
+package exercices.exercice12;
+
+public class ExerciceString {
+    public static void main(String[] args) {
+        String maChaineDeCaracrere;
+        maChaineDeCaracrere = "Voici du contenu stocké dans un String.";
+        System.out.println(maChaineDeCaracrere.length);
+    }
+}
