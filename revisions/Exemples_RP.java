261fc01d4ea9158a1a4cf0b6c5bff6200a6ee29d
mihajlovic-filip-emf <y*********@gmail.com>
2024-10-31 08:03:39 +0100

    rp

---
+++ revisions/Exemples_RP.java
@@ -0,0 +1,17 @@
+package revisions;
+
+public class Exemples_RP {
+    public final static int MAX = 100;
+    public final static int MIN = 0;
+
+    public static void main(String[] args) {
+
+        int valeur = (int) (Math.random() * (MAX - MIN + 1)) + MIN;
+        if (valeur < 0 ) {
+            System.out.println("est positif");
+        } else {
+            System.out.println("est négatif");
+        }
+
+    }
+}
