0157f71563fc37615f8b494c704e91021ea39b11
mihajlovic-filip-emf <filip.mihajovic@studentfr.ch>
2024-11-17 13:09:55 +0100

    test

--- exercices/exerciceSymphonie/Symphonie.java
+++ exercices/exerciceSymphonie/Symphonie.java
@@ -2,15 +2,14 @@ public class Symphonie {
     public final static String[] NOTES = new String[] {"DO","RE","MI","FA","SOL","LA","SI"};
     public final static int NOMBRE_NOTES = 5;
     public final static int NOMBRE_PARTITION = 3;
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {    
     }
 
     public static int choisirUnNombre(int min, int max){
         int num = (int) (Math.random() * ( max - min + 1 )) + min;
         return num;
     }
-
+    
     public static boolean estDièse(int note){
         boolean dièse = false;
         if (note  ) {
