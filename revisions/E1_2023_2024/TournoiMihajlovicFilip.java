4d98fc913b082685cc1a06e8f9fa5f1937fff99c
mihajlovic-filip-emf <y*********@gmail.com>
2024-11-21 13:06:53 +0100

    E1 2023-24

--- revisions/E1_2023_2024/TournoiMihajlovicFilip.java
+++ revisions/E1_2023_2024/TournoiMihajlovicFilip.java
@@ -2,18 +2,43 @@
  * TournoiMihajlovicFilip
  */
 public class TournoiMihajlovicFilip {
-    public final static String[] EQUIPES = new String[]{"FC Richemont","FC Central", "FC Schoenberg","FC Beauregard", "Team AFF", "Etoile Sport" } ;
+    public final static String[] EQUIPES = new String[] { "FC Richemont", "FC Central", "FC Schoenberg",
+            "FC Beauregard", "Team AFF", "Etoile Sport" };
     public final static int MAX_GOALS = 10;
-
+    public static int[] pointsEquipes = new int[6];
     public static void main(String[] args) {
-        int[] pointsEquipes = new int[6];
-
         for (int i = 0; i < EQUIPES.length; i++) {
-            
+            simuleMatch(i, i + 1);
+            if (i == EQUIPES.length - 1) {
+                simuleMatch(i, 0);
+            }
+        }
+        System.out.println("-------------------------------------");
+        for (int i = 0; i < pointsEquipes.length; i++) {
+            System.out.println("l'équipe " + EQUIPES[i] + " a " + pointsEquipes[i] + " points");
+        }
+        System.out.println("-------------------------------------");
+        int[] gagnants;
+    }
+
+    public static int genererNombre(int min, int max){
+        int num = (int) (Math.random() * ( max - min + 1 )) + min;
+        return num;
+    }
+
+    public static int[] simuleMatch(int index1, int index2){
+        int scoreEquipe1 = genererNombre(0, MAX_GOALS);
+        int scoreEquipe2 = simuleMatch(0, MAX_GOALS);
+        if (scoreEquipe1 > scoreEquipe2) {
+            pointsEquipes[index1]+=3;
+            System.out.println("L'équipe " EQUIPES[index1]);
+        }else
+        if (scoreEquipe2> scoreEquipe1) {
+            pointsEquipes[index2]+=3;
         }
     }
 
-    public static int simuleMatch(){
-        
+    public static void trouveGagnant( ){
+
     }
 }
